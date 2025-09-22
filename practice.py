db1bf006028f64a850533b61f20e943ef572c9e0
DeepaRaghu <rsumanthreddy03@gmail.com>
2025-09-22 12:44:35 +0530

    file 2 interview list and strings and collections

--- practice.py
+++ practice.py
@@ -1,15 +1,28 @@
-#while loop
-count=1
-while count<=10:
-    print("hello")
-    count+=1
-print("count" ,count)
 
-#while
-i=1
-while i<=100:
-    print(i)
-    i+=1
+def greet():
+    print("hello")
+def surename():
+    print("lion")
+greet()
+surename()
+
+def greet(name,age):
+    print("iam ",name)
+    print("my age is ",age)
+greet("john",56)    
+
+# function with return value
+def Square(n):
+    return n * n
+result = Square(6)
+print("Square:" ,result)
+
+# math operations
+def math_operations(a, b):
+    return a+b,a-b,a*b,a/b
+add,sub,mul,div  = math_operations(10, 5)
+print("Add:", add, "Sub:", sub, "Mul:", mul, "Div:", div)    
+
 
 
 

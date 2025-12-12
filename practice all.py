3a5afc55cdaf0e57509ab589ad4bfd9c59df3061
DeepaRaghu <rsumanthreddy03@gmail.com>
2025-12-12 10:32:37 +0530

    practice all python,py

--- practice all.py
+++ practice all.py
@@ -681,6 +681,12 @@ try:
     print(len(123))
 except TypeError:
     print("Error: length not possible for numbers")
+#41
+try:
+    d = {"a": 1}
+    print(d["b"])
+except KeyError:
+    print("Error: Key not found in dictionary")
 
 
 

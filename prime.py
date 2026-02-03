692048c1f606d3b29c62853d6a2e6c104a10f579
DeepaRaghu <rsumanthreddy03@gmail.com>
2026-02-03 09:31:18 +0530

    prime.py

--- prime.py
+++ prime.py
@@ -54,3 +54,17 @@ else:
         i += 1
     else:
         print("Prime")
+#6
+import math
+
+num = int(input("Enter number: "))
+
+if num <= 1:
+    print("Not Prime")
+else:
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")

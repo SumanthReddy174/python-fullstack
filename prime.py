a8d0217e333e09a38123971225eb02cd31e4791a
DeepaRaghu <rsumanthreddy03@gmail.com>
2026-02-13 10:37:04 +0530

    prime.py

--- prime.py
+++ prime.py
@@ -143,4 +143,16 @@ if flag:
     print("Prime")
 else:
     print("Not Prime")
+#14
+count = 0
+num = 2
+
+while count < 10:
+    for i in range(2, num):
+        if num % i == 0:
+            break
+    else:
+        print(num)
+        count += 1
+    num += 1
 

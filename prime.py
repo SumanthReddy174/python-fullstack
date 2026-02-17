fd218f0c195ae3d493aaa8f258d2f10f3885c6f0
DeepaRaghu <rsumanthreddy03@gmail.com>
2026-02-17 10:39:51 +0530

    prime.py

--- prime.py
+++ prime.py
@@ -204,3 +204,14 @@ for num in nums:
                 break
         else:
             print(num, "Prime")
+#19
+def prime(n):
+    if n <= 1:
+        return "Not Prime"
+    for i in range(2, n):
+        if n % i == 0:
+            return "Not Prime"
+    return "Prime"
+
+num = int(input("Enter number: "))
+print(prime(num))

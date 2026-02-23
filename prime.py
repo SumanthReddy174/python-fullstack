3c8f61bce4b0b3f914466051929e4b8fe8e0871f
DeepaRaghu <rsumanthreddy03@gmail.com>
2026-02-23 11:01:42 +0530

    prime.py

--- prime.py
+++ prime.py
@@ -263,3 +263,15 @@ for num in range(2, 30):
         primes.append(num)
 
 print(primes)
+#24
+def check_prime(n, i=2):
+    if n <= 1:
+        return False
+    if i == n:
+        return True
+    if n % i == 0:
+        return False
+    return check_prime(n, i+1)
+
+num = int(input("Enter number: "))
+print("Prime" if check_prime(num) else "Not Prime")
